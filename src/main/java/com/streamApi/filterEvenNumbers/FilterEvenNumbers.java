69f36f634a0f07f01f1ef57b98aa55ee41d9af05
emirtotic <emirtotic@gmail.com>
2024-10-06 20:07:11 +0200

    Filter Even Numbers

---
+++ src/main/java/com/streamApi/filterEvenNumbers/FilterEvenNumbers.java
@@ -0,0 +1,22 @@
+package com.streamApi.filterEvenNumbers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterEvenNumbers {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        System.out.println(numbers);
+
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(evenNumbers);
+
+    }
+
+}
